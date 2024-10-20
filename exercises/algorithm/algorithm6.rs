67a493812d5717f5682e55eb25ac5c53e02f6a9d
xiaoyvhuv <xiaoyvhuv@sina.com>
2024-10-20 16:54:43 +0800

    all get!

--- exercises/algorithm/algorithm6.rs
+++ exercises/algorithm/algorithm6.rs
@@ -1,9 +1,87 @@
+// /*
+// 	dfs
+// 	This problem requires you to implement a basic DFS traversal
+// */
+
+
+// use std::collections::HashSet;
+
+// struct Graph {
+//     adj: Vec<Vec<usize>>, 
+// }
+
+// impl Graph {
+//     fn new(n: usize) -> Self {
+//         Graph {
+//             adj: vec![vec![]; n],
+//         }
+//     }
+
+//     fn add_edge(&mut self, src: usize, dest: usize) {
+//         self.adj[src].push(dest);
+//         self.adj[dest].push(src); 
+//     }
+
+//     fn dfs_util(&self, v: usize, visited: &mut HashSet<usize>, visit_order: &mut Vec<usize>) {
+//         //TODO
+//     }
+
+//     // Perform a depth-first search on the graph, return the order of visited nodes
+//     fn dfs(&self, start: usize) -> Vec<usize> {
+//         let mut visited = HashSet::new();
+//         let mut visit_order = Vec::new(); 
+//         self.dfs_util(start, &mut visited, &mut visit_order);
+//         visit_order
+//     }
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn test_dfs_simple() {
+//         let mut graph = Graph::new(3);
+//         graph.add_edge(0, 1);
+//         graph.add_edge(1, 2);
+
+//         let visit_order = graph.dfs(0);
+//         assert_eq!(visit_order, vec![0, 1, 2]);
+//     }
+
+//     #[test]
+//     fn test_dfs_with_cycle() {
+//         let mut graph = Graph::new(4);
+//         graph.add_edge(0, 1);
+//         graph.add_edge(0, 2);
+//         graph.add_edge(1, 2);
+//         graph.add_edge(2, 3);
+//         graph.add_edge(3, 3); 
+
+//         let visit_order = graph.dfs(0);
+//         assert_eq!(visit_order, vec![0, 1, 2, 3]);
+//     }
+
+//     #[test]
+//     fn test_dfs_disconnected_graph() {
+//         let mut graph = Graph::new(5);
+//         graph.add_edge(0, 1);
+//         graph.add_edge(0, 2);
+//         graph.add_edge(3, 4); 
+
+//         let visit_order = graph.dfs(0);
+//         assert_eq!(visit_order, vec![0, 1, 2]); 
+//         let visit_order_disconnected = graph.dfs(3);
+//         assert_eq!(visit_order_disconnected, vec![3, 4]); 
+//     }
+// }
+
 /*
 	dfs
 	This problem requires you to implement a basic DFS traversal
 */
 
-// I AM NOT DONE
+
 use std::collections::HashSet;
 
 struct Graph {
@@ -24,6 +102,14 @@ impl Graph {
 
     fn dfs_util(&self, v: usize, visited: &mut HashSet<usize>, visit_order: &mut Vec<usize>) {
         //TODO
+        visited.insert(v);  // 标记当前节点为已访问
+        visit_order.push(v);  // 将当前节点加入访问顺序列表
+
+        for &i in &self.adj[v] {
+            if !visited.contains(&i) {  // 对于每一个未访问的邻接节点
+                self.dfs_util(i, visited, visit_order);  // 递归访问
+            }
+        }
     }
 
     // Perform a depth-first search on the graph, return the order of visited nodes
@@ -75,4 +161,3 @@ mod tests {
         assert_eq!(visit_order_disconnected, vec![3, 4]); 
     }
 }
-
--- exercises/algorithm/algorithm9.rs
+++ exercises/algorithm/algorithm9.rs
@@ -1,154 +1,275 @@
+// /*
+// 	heap
+// 	This question requires you to implement a binary heap function
+// */
+
+
+// use std::cmp::Ord;
+// use std::default::Default;
+
+// pub struct Heap<T>
+// where
+//     T: Default,
+// {
+//     count: usize,
+//     items: Vec<T>,
+//     comparator: fn(&T, &T) -> bool,
+// }
+
+// impl<T> Heap<T>
+// where
+//     T: Default,
+// {
+//     pub fn new(comparator: fn(&T, &T) -> bool) -> Self {
+//         Self {
+//             count: 0,
+//             items: vec![T::default()],
+//             comparator,
+//         }
+//     }
+
+//     pub fn len(&self) -> usize {
+//         self.count
+//     }
+
+//     pub fn is_empty(&self) -> bool {
+//         self.len() == 0
+//     }
+
+//     pub fn add(&mut self, value: T) {
+//         //TODO
+//     }
+
+//     fn parent_idx(&self, idx: usize) -> usize {
+//         idx / 2
+//     }
+
+//     fn children_present(&self, idx: usize) -> bool {
+//         self.left_child_idx(idx) <= self.count
+//     }
+
+//     fn left_child_idx(&self, idx: usize) -> usize {
+//         idx * 2
+//     }
+
+//     fn right_child_idx(&self, idx: usize) -> usize {
+//         self.left_child_idx(idx) + 1
+//     }
+
+//     fn smallest_child_idx(&self, idx: usize) -> usize {
+//         //TODO
+// 		0
+//     }
+// }
+
+// impl<T> Heap<T>
+// where
+//     T: Default + Ord,
+// {
+//     /// Create a new MinHeap
+//     pub fn new_min() -> Self {
+//         Self::new(|a, b| a < b)
+//     }
+
+//     /// Create a new MaxHeap
+//     pub fn new_max() -> Self {
+//         Self::new(|a, b| a > b)
+//     }
+// }
+
+// impl<T> Iterator for Heap<T>
+// where
+//     T: Default,
+// {
+//     type Item = T;
+
+//     fn next(&mut self) -> Option<T> {
+//         //TODO
+// 		None
+//     }
+// }
+
+// pub struct MinHeap;
+
+// impl MinHeap {
+//     #[allow(clippy::new_ret_no_self)]
+//     pub fn new<T>() -> Heap<T>
+//     where
+//         T: Default + Ord,
+//     {
+//         Heap::new(|a, b| a < b)
+//     }
+// }
+
+// pub struct MaxHeap;
+
+// impl MaxHeap {
+//     #[allow(clippy::new_ret_no_self)]
+//     pub fn new<T>() -> Heap<T>
+//     where
+//         T: Default + Ord,
+//     {
+//         Heap::new(|a, b| a > b)
+//     }
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     #[test]
+//     fn test_empty_heap() {
+//         let mut heap = MaxHeap::new::<i32>();
+//         assert_eq!(heap.next(), None);
+//     }
+
+//     #[test]
+//     fn test_min_heap() {
+//         let mut heap = MinHeap::new();
+//         heap.add(4);
+//         heap.add(2);
+//         heap.add(9);
+//         heap.add(11);
+//         assert_eq!(heap.len(), 4);
+//         assert_eq!(heap.next(), Some(2));
+//         assert_eq!(heap.next(), Some(4));
+//         assert_eq!(heap.next(), Some(9));
+//         heap.add(1);
+//         assert_eq!(heap.next(), Some(1));
+//     }
+
+//     #[test]
+//     fn test_max_heap() {
+//         let mut heap = MaxHeap::new();
+//         heap.add(4);
+//         heap.add(2);
+//         heap.add(9);
+//         heap.add(11);
+//         assert_eq!(heap.len(), 4);
+//         assert_eq!(heap.next(), Some(11));
+//         assert_eq!(heap.next(), Some(9));
+//         assert_eq!(heap.next(), Some(4));
+//         heap.add(1);
+//         assert_eq!(heap.next(), Some(2));
+//     }
+// }
+
+
 /*
-	heap
-	This question requires you to implement a binary heap function
+	queue
+	This question requires you to use queues to implement the functionality of the stac
 */
-// I AM NOT DONE
 
-use std::cmp::Ord;
-use std::default::Default;
 
-pub struct Heap<T>
-where
-    T: Default,
-{
-    count: usize,
-    items: Vec<T>,
-    comparator: fn(&T, &T) -> bool,
+#[derive(Debug)]
+pub struct Queue<T> {
+    elements: Vec<T>,
 }
 
-impl<T> Heap<T>
-where
-    T: Default,
-{
-    pub fn new(comparator: fn(&T, &T) -> bool) -> Self {
-        Self {
-            count: 0,
-            items: vec![T::default()],
-            comparator,
+impl<T> Queue<T> {
+    pub fn new() -> Queue<T> {
+        Queue {
+            elements: Vec::new(),
         }
     }
 
-    pub fn len(&self) -> usize {
-        self.count
+    pub fn enqueue(&mut self, value: T) {
+        self.elements.push(value)
+    }
+
+    pub fn dequeue(&mut self) -> Result<T, &str> {
+        if !self.elements.is_empty() {
+            Ok(self.elements.remove(0usize))
+        } else {
+            Err("Queue is empty")
+        }
+    }
+
+    pub fn peek(&self) -> Result<&T, &str> {
+        match self.elements.first() {
+            Some(value) => Ok(value),
+            None => Err("Queue is empty"),
+        }
+    }
+
+    pub fn size(&self) -> usize {
+        self.elements.len()
     }
 
     pub fn is_empty(&self) -> bool {
-        self.len() == 0
-    }
-
-    pub fn add(&mut self, value: T) {
-        //TODO
-    }
-
-    fn parent_idx(&self, idx: usize) -> usize {
-        idx / 2
-    }
-
-    fn children_present(&self, idx: usize) -> bool {
-        self.left_child_idx(idx) <= self.count
-    }
-
-    fn left_child_idx(&self, idx: usize) -> usize {
-        idx * 2
-    }
-
-    fn right_child_idx(&self, idx: usize) -> usize {
-        self.left_child_idx(idx) + 1
-    }
-
-    fn smallest_child_idx(&self, idx: usize) -> usize {
-        //TODO
-		0
+        self.elements.is_empty()
     }
 }
 
-impl<T> Heap<T>
-where
-    T: Default + Ord,
+impl<T> Default for Queue<T> {
+    fn default() -> Queue<T> {
+        Queue {
+            elements: Vec::new(),
+        }
+    }
+}
+
+pub struct myStack<T>
 {
-    /// Create a new MinHeap
-    pub fn new_min() -> Self {
-        Self::new(|a, b| a < b)
-    }
-
-    /// Create a new MaxHeap
-    pub fn new_max() -> Self {
-        Self::new(|a, b| a > b)
-    }
+	//TODO
+	q1:Queue<T>,
+	q2:Queue<T>
 }
-
-impl<T> Iterator for Heap<T>
-where
-    T: Default,
-{
-    type Item = T;
-
-    fn next(&mut self) -> Option<T> {
+impl<T> myStack<T> {
+    pub fn new() -> Self {
+        Self {
+			//TODO
+			q1:Queue::<T>::new(),
+			q2:Queue::<T>::new()
+        }
+    }
+    pub fn push(&mut self, elem: T) {
         //TODO
-		None
+        // 元素首先入队到q2
+        self.q2.enqueue(elem);
+
+        // 将q1中的所有元素转移到q2
+        while !self.q1.is_empty() {
+            if let Ok(data) = self.q1.dequeue() {
+                self.q2.enqueue(data);
+            }
+        }
+
+        // 交换q1和q2
+        std::mem::swap(&mut self.q1, &mut self.q2);
     }
-}
-
-pub struct MinHeap;
-
-impl MinHeap {
-    #[allow(clippy::new_ret_no_self)]
-    pub fn new<T>() -> Heap<T>
-    where
-        T: Default + Ord,
-    {
-        Heap::new(|a, b| a < b)
+    pub fn pop(&mut self) -> Result<T, &str> {
+        //TODO
+        if self.q1.is_empty() {
+            return Err("Stack is empty");
+        }
+        self.q1.dequeue()
     }
-}
-
-pub struct MaxHeap;
-
-impl MaxHeap {
-    #[allow(clippy::new_ret_no_self)]
-    pub fn new<T>() -> Heap<T>
-    where
-        T: Default + Ord,
-    {
-        Heap::new(|a, b| a > b)
+    pub fn is_empty(&self) -> bool {
+		//TODO
+        self.q1.is_empty()
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    #[test]
-    fn test_empty_heap() {
-        let mut heap = MaxHeap::new::<i32>();
-        assert_eq!(heap.next(), None);
-    }
-
-    #[test]
-    fn test_min_heap() {
-        let mut heap = MinHeap::new();
-        heap.add(4);
-        heap.add(2);
-        heap.add(9);
-        heap.add(11);
-        assert_eq!(heap.len(), 4);
-        assert_eq!(heap.next(), Some(2));
-        assert_eq!(heap.next(), Some(4));
-        assert_eq!(heap.next(), Some(9));
-        heap.add(1);
-        assert_eq!(heap.next(), Some(1));
-    }
-
-    #[test]
-    fn test_max_heap() {
-        let mut heap = MaxHeap::new();
-        heap.add(4);
-        heap.add(2);
-        heap.add(9);
-        heap.add(11);
-        assert_eq!(heap.len(), 4);
-        assert_eq!(heap.next(), Some(11));
-        assert_eq!(heap.next(), Some(9));
-        assert_eq!(heap.next(), Some(4));
-        heap.add(1);
-        assert_eq!(heap.next(), Some(2));
-    }
+	use super::*;
+	
+	#[test]
+	fn test_queue(){
+		let mut s = myStack::<i32>::new();
+		assert_eq!(s.pop(), Err("Stack is empty"));
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        assert_eq!(s.pop(), Ok(3));
+        assert_eq!(s.pop(), Ok(2));
+        s.push(4);
+        s.push(5);
+        assert_eq!(s.is_empty(), false);
+        assert_eq!(s.pop(), Ok(5));
+        assert_eq!(s.pop(), Ok(4));
+        assert_eq!(s.pop(), Ok(1));
+        assert_eq!(s.pop(), Err("Stack is empty"));
+        assert_eq!(s.is_empty(), true);
+	}
 }
